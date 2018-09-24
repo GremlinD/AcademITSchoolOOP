9befa6775c9c6ee5e3e68f3b9234fefd6fe1b606
Кирилл <krivolutsky.k@mail.ru>
2018-09-24 19:49:00 +0700

    Задача Range

---
+++ Shapes/ru.krivolutskyWork.2/src/Square.java
@@ -0,0 +1,27 @@
+public class Square implements Shape {
+    private double length;
+
+    Square(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getWidth() {
+        return length;
+    }
+
+    @Override
+    public double getHeight() {
+        return length;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(length, 2);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * length;
+    }
+}
