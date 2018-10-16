a0c217c6bafc0e026da43f1b8d3a1ba55667efb2
Кирилл <krivolutsky.k@mail.ru>
2018-10-16 14:04:14 +0700

    Graph.

--- Graph/src/ru/krivolutsky/work11/classes/Graph.java
+++ Graph/src/ru/krivolutsky/work11/classes/Graph.java
@@ -13,13 +13,47 @@ public class Graph {
     };
     private boolean visit[] = new boolean[graph.length];
 
-    public void walkWide(){
+    public void walkWide() {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        visit[0] = true;
-        while (queue.peek() != null){
-            for (Integer i = 0; i < graph[queue.element()]; i++) {
+        if (graph.length != 0) {
+            queue.add(0);
+        }
+        while (queue.peek() != null) {
+            if (visit[queue.element()]) {
+                queue.remove();
+                continue;
+            }
+            for (int i = 0; i < graph.length; i++) {
+                if (queue.element() != i) {
+                    if (graph[queue.element()][i] == 1 && !visit[i]) {
+                        queue.add(i);
+                    }
+                }
+            }
+            visit[queue.element()] = true;
+            System.out.println(queue.element());
+            queue.remove();
+        }
+        visit = new boolean[graph.length];
+    }
 
+    public void Visit(int index) {
+        if (graph.length == 0) {
+            
+        }
+        if (index < 0 || index >= graph.length) {
+            throw new IndexOutOfBoundsException("Индекс выходит за границы графа.");
+        }
+        if (visit[index]) {
+            return;
+        }
+        System.out.println(index);
+        visit[index] = true;
+        for (int i = 0; i < graph.length; i++) {
+            if (i != index) {
+                if (graph[index][i] == 1 && !visit[i]) {
+                    Visit(i);
+                }
             }
         }
     }
---
+++ Graph/src/ru/krivolutsky/work11/main/Main.java
@@ -0,0 +1,15 @@
+package ru.krivolutsky.work11.main;
+
+import ru.krivolutsky.work11.classes.Graph;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.walkWide();
+        try {
+            graph.Visit(0);
+        }catch (IndexOutOfBoundsException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
