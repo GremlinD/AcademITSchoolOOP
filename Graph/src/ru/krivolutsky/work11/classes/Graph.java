8250c9ec170d20848dee238bcfb19a2cc0e84442
Kirill krivolutsky <krivolutsky.k@mail.ru>
2018-10-19 13:39:48 +0700

    Graph исправления.

--- Graph/src/ru/krivolutsky/work11/classes/Graph.java
+++ Graph/src/ru/krivolutsky/work11/classes/Graph.java
@@ -5,16 +5,10 @@ import java.util.Queue;
 import java.util.function.Consumer;
 
 public class Graph {
-    private int[][] graph = new int[][]{
-            {0, 1, 0, 0, 1},
-            {1, 0, 1, 1, 0},
-            {0, 1, 0, 0, 0},
-            {0, 1, 0, 0, 1},
-            {1, 0, 0, 1, 0}
-    };
+    private int[][] graph;
 
-    public int getSize() {
-        return graph.length;
+    public Graph(int[][] graph) {
+        this.graph = graph;
     }
 
     public void walkWide(Consumer<Integer> consumer) {
@@ -23,39 +17,65 @@ public class Graph {
             queue.add(0);
         }
         boolean[] visit = new boolean[graph.length];
-        while (queue.peek() != null) {
-            if (visit[queue.element()]) {
+        int visitCount = 0;
+        while (!queue.isEmpty() || visitCount < visit.length) {
+            if (queue.isEmpty()) {
+                for (int i = 1; i < visit.length; i++) {
+                    if (!visit[i]) {
+                        queue.add(i);
+                        break;
+                    }
+                }
+            }
+            int element = queue.element();
+            if (visit[element]) {
                 queue.remove();
                 continue;
             }
             for (int i = 0; i < graph.length; i++) {
-                if (queue.element() != i) {
-                    if (graph[queue.element()][i] == 1 && !visit[i]) {
+                if (element != i) {
+                    if (graph[element][i] == 1 && !visit[i]) {
                         queue.add(i);
                     }
                 }
             }
-            visit[queue.element()] = true;
-            consumer.accept(queue.element());
+            visit[element] = true;
+            visitCount++;
+            consumer.accept(element);
             queue.remove();
         }
     }
 
-    public void visit(int index, boolean[] visit, Consumer<Integer> consumer) {
+    public void visit(Consumer<Integer> consumer) {
+        boolean[] visit = new boolean[graph.length];
+        int visitCount = 0;
+        while (visitCount < visit.length) {
+            for (int i = 0; i < visit.length; i++) {
+                if (!visit[i]) {
+                    visitCount = visit(i, visit, consumer, visitCount);
+                    break;
+                }
+            }
+        }
+    }
+
+    private int visit(int index, boolean[] visit, Consumer<Integer> consumer, int visitCount) {
         if (index < 0 || index >= graph.length) {
             throw new IndexOutOfBoundsException("Индекс выходит за границы графа.");
         }
         if (visit[index]) {
-            return;
+            return visitCount;
         }
         consumer.accept(index);
         visit[index] = true;
+        visitCount++;
         for (int i = 0; i < graph.length; i++) {
             if (i != index) {
                 if (graph[index][i] == 1 && !visit[i]) {
-                    visit(i, visit, consumer);
+                    visitCount = visit(i, visit, consumer, visitCount);
                 }
             }
         }
+        return visitCount;
     }
 }
--- Graph/src/ru/krivolutsky/work11/main/Main.java
+++ Graph/src/ru/krivolutsky/work11/main/Main.java
@@ -6,12 +6,18 @@ import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-        Graph graph = new Graph();
+        Graph graph = new Graph(new int[][]{
+                {0, 0, 1, 1, 0},
+                {0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0}
+        });
         Consumer<Integer> consumer = System.out::println;
         graph.walkWide(consumer);
         System.out.println();
         try {
-            graph.visit(0, new boolean[graph.getSize()], consumer);
+            graph.visit(consumer);
         } catch (IndexOutOfBoundsException exception) {
             System.out.println(exception.getMessage());
         }
