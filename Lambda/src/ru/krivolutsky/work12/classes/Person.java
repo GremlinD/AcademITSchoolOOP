a1ae2e2da0a3e0b8f42ce5aa13a1b17f06055f2c
Кирилл <krivolutsky.k@mail.ru>
2018-10-26 20:18:22 +0700

    Lambda

---
+++ Lambda/src/ru/krivolutsky/work12/classes/Person.java
@@ -0,0 +1,44 @@
+package ru.krivolutsky.work12.classes;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (o == null || o.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Person person = (Person) o;
+
+        return Objects.equals(person.name, name);
+    }
+}
---
+++ Lambda/src/ru/krivolutsky/work12/main/Main.java
@@ -0,0 +1,35 @@
+package ru.krivolutsky.work12.main;
+
+import ru.krivolutsky.work12.classes.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("q", 12));
+        people.add(new Person("w", 43));
+        people.add(new Person("e", 65));
+        people.add(new Person("q", 13));
+        people.add(new Person("w", 98));
+        people.add(new Person("e", 23));
+        people.add(new Person("q", 54));
+        people.add(new Person("w", 34));
+        people.add(new Person("e", 83));
+        people.add(new Person("q", 56));
+
+        List<Person> uniquePeople = people.stream().distinct().collect(Collectors.toList());
+        System.out.println(uniquePeople.stream().map(Person::getName).collect(Collectors.joining(", ", "Имена: ", ".")));
+
+        List<Person> minor = people.stream().filter(p -> p.getAge() < 18).collect(Collectors.toList());
+        System.out.println(minor.stream().mapToDouble(Person::getAge).average().getAsDouble());
+
+        Map<String, Double> averageByNames = people.stream().collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
+
+        List<Person> people2045 = people.stream().filter(p -> p.getAge() > 18 && p.getAge() < 45).sorted((p1, p2) -> p2.getAge()-p1.getAge()).collect(Collectors.toList());
+        System.out.println(people2045.stream().map(Person::getName).collect(Collectors.joining(", ", "Возраст: ", ".")));
+    }
+}
