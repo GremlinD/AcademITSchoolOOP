f16bc550ef066b5c0e06060c27011217b254a0c3
Kirill krivolutsky <krivolutsky.k@mail.ru>
2018-12-02 10:20:46 +0700

    TC

--- TempreatureConversion/src/ru/krivolutsky/work15/main/Main.java
+++ TempreatureConversion/src/ru/krivolutsky/work15/main/Main.java
@@ -1,20 +1,30 @@
 package ru.krivolutsky.work15.main;
 
+import ru.krivolutsky.work15.classes.TemperatureConversion;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
+    private static boolean isNumeric(String str) {
+        try {
+            double d = Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame mainFrame = new JFrame("Перевод температур");
             mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            mainFrame.setSize(206, 130);
+            mainFrame.setSize(220, 130);
 
             JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-
             mainFrame.add(panel);
-
-
             JComboBox comboBox = new JComboBox();
             comboBox.setSize(400, 20);
             comboBox.addItem("Кельвин");
@@ -29,17 +39,65 @@ public class Main {
             comboBox2.addItem("Фаренгейт");
             panel.add(comboBox2);
 
-            JTextField textField = new JTextField("qweqwe");
+            JTextField textField = new JTextField();
             textField.setColumns(8);
             panel.add(textField);
 
             JTextField textField2 = new JTextField();
             textField2.setColumns(8);
+            textField2.setEnabled(false);
             panel.add(textField2);
 
             JButton button = new JButton("Ответ");
+            button.addActionListener(e -> {
+                int scale = comboBox.getSelectedIndex();
+                int scale2 = comboBox2.getSelectedIndex();
+                String temperature = textField.getText();
+                if (isNumeric(temperature)) {
+                    switch (scale) {
+                        case 0:
+                            switch (scale2) {
+                                case 0:
+                                    textField2.setText(temperature);
+                                    break;
+                                case 1:
+                                    textField2.setText(Double.toString(TemperatureConversion.kelvinToCelsius(Double.parseDouble(temperature))));
+                                    break;
+                                case 2:
+                                    textField2.setText(Double.toString(TemperatureConversion.kelvinToFahrenheit(Double.parseDouble(temperature))));
+                                    break;
+                            }
+                            break;
+                        case 1:
+                            switch (scale2) {
+                                case 0:
+                                    textField2.setText(Double.toString(TemperatureConversion.celsiusToKelvin(Double.parseDouble(temperature))));
+                                    break;
+                                case 1:
+                                    textField2.setText(temperature);
+                                    break;
+                                case 2:
+                                    textField2.setText(Double.toString(TemperatureConversion.celsiusToFahrenheit(Double.parseDouble(temperature))));
+                                    break;
+                            }
+                            break;
+                        case 2:
+                            switch (scale2) {
+                                case 0:
+                                    textField2.setText(Double.toString(TemperatureConversion.fahrenheitToKelvin(Double.parseDouble(temperature))));
+                                    break;
+                                case 1:
+                                    textField2.setText(Double.toString(TemperatureConversion.fahrenheitToCelsius(Double.parseDouble(temperature))));
+                                    break;
+                                case 2:
+                                    textField2.setText(temperature);
+                                    break;
+                            }
+                            break;
+                    }
+                }
+            });
             panel.add(button);
-
             mainFrame.setVisible(true);
         });
     }
